30483019f674a1c377638dd93ea357621cdb8400
porotogus <gustavocarrena@outlook.com>
2022-07-30 13:47:53 -0300

    md

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,10 +2,6 @@ import styles from "./loader.module.scss";
 
 export const Loader = () => {
   return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles.spinner} spinner-grow`} role="status">
-        <span className={`${styles.loading}`}>Loading...</span>
-      </div>
-    </div>
+    <div className={`${styles.loading} animate__animated animate__flash`}>Loading...</div>
   );
 };
